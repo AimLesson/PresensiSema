b0d486c5a9b74a34f9f9c5e4cce8cdb1aa5ef349
AimLesson <113356516+AimLesson@users.noreply.github.com>
2024-11-01 17:16:07 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,10 +7,54 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
+
+
+            <div class="relative overflow-x-auto">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Nama
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                NIM
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                TimeStamp
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Jenis Presensi
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Gambar
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php
+                            $presensi = \App\Models\Presensi::all();
+                        @endphp
+                        @foreach ($presensi as $p)
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row"
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ $p->nama }}
+                                </th>
+                                <td class="px-6 py-4">
+                                    {{ $p->nim }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ $p->timestamp }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ $p->jenis }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ $p->kamera }}
+                                </td>
+                            </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
